309d35d453ca8c4bb6e700c0900cbc10a7a1bf0f
王焕 <2323666215@qq.com>
2021-09-18 10:30:40 +0800

    fix: bind decorate to root node #WIK-4803

--- src/plugin/use-cursors.ts
+++ src/plugin/use-cursors.ts
@@ -1,4 +1,4 @@
-import { NodeEntry, Path, Range, Text } from 'slate';
+import { NodeEntry, Range } from 'slate';
 import { Cursor, CursorInfo } from '../model';
 import { relativePositionToAbsolutePosition } from '../cursor/utils';
 import { CursorEditor } from './cursor-editor';
@@ -38,39 +38,22 @@ export const useCursors = (
       })
       .filter((cursor) => cursor.anchor && cursor.focus) as Cursor[];
     
-    const decorate = ([node, path]: NodeEntry) => {
+    const decorate = ([, path]: NodeEntry) => {
       const ranges: (Range & CursorInfo)[] = [];
-      if (Text.isText(node) && cursors?.length) {
+      if (path && path.length === 1 && cursors?.length) {
         cursors.forEach((cursor) => {
           if (Range.includes(cursor, path)) {
             const { focus, anchor, data } = cursor;
 
-            const isFocusNode = Path.equals(focus.path, path);
-            const isAnchorNode = Path.equals(anchor.path, path);
             const isForward = Range.isForward({ anchor, focus });
 
             ranges.push({
               data,
               isForward,
-              isCaret: isFocusNode,
-              anchor: {
-                path,
-                // eslint-disable-next-line no-nested-ternary
-                offset: isAnchorNode
-                  ? anchor.offset
-                  : isForward
-                    ? 0
-                    : node.text.length
-              },
-              focus: {
-                path,
-                // eslint-disable-next-line no-nested-ternary
-                offset: isFocusNode
-                  ? focus.offset
-                  : isForward
-                    ? node.text.length
-                    : 0
-              }
+              originAnchorPath: anchor.path,
+              originFocusPath: focus.path,
+              anchor,
+              focus
             });
           }
         });
